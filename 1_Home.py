55a5376001c71dd234e872689d95e5b0bec9f8ea
MohamHussein <MohamHussein@MOBCORP.INTRT.COM>
2026-06-29 02:43:35 +0300

    Fixed import order to successfully block thread collisions

--- 1_Home.py
+++ 1_Home.py
@@ -1,12 +1,11 @@
-import scipy
-import streamlit as st
 import os
 os.environ['OMP_NUM_THREADS'] = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
 os.environ['NUMEXPR_NUM_THREADS'] = '1'
-
+import scipy
+import streamlit as st
 # -----------------------------------------------------------------------------
 # 1. Page Configuration & Logo
 # -----------------------------------------------------------------------------
