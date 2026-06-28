ee8e2fe09f76ee49a726fe1afaee1c1b67aebb19
MohamHussein <MohamHussein@MOBCORP.INTRT.COM>
2026-06-29 02:37:30 +0300

    Fixed OpenMP and BLAS threading segfault

--- 1_Home.py
+++ 1_Home.py
@@ -1,5 +1,11 @@
 import scipy
 import streamlit as st
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
 
 # -----------------------------------------------------------------------------
 # 1. Page Configuration & Logo
